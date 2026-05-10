e1feaf890b7d194056749d93ccda44afe6986131
WongPouI-Polly <dc22657@um.edu.mo>
2026-05-10 22:23:39 +0800

    Wong Pou I DC226572

---
+++ CISC3003-Practice09-2026MAR27/public/left.inc.php
@@ -0,0 +1,17 @@
+<div class="mdl-layout__drawer">
+    <div class="profile" style="padding: 16px; text-align: center;">
+        <img src="images/profile.jpg" class="avatar" style="width: 80px; height: 80px; border-radius: 50%;">
+        <h4 style="margin: 10px 0 5px;">John Locke</h4>
+        <span style="font-size: 12px;">johnlocke@example.com</span>
+    </div>
+    <nav class="mdl-navigation">
+        <a class="mdl-navigation__link" href=""><i class="material-icons">dashboard</i> Dashboard</a>
+        <a class="mdl-navigation__link" href=""><i class="material-icons">message</i> Messages</a>
+        <a class="mdl-navigation__link" href=""><i class="material-icons">event</i> Tasks</a>
+        <a class="mdl-navigation__link" href=""><i class="material-icons">call</i> Orders</a>
+        <a class="mdl-navigation__link" href=""><i class="material-icons">settings</i> Configure</a>
+        <a class="mdl-navigation__link" href=""><i class="material-icons">view_list</i> Catalog</a>
+        <a class="mdl-navigation__link" href=""><i class="material-icons">contacts</i> Customers</a>
+        <a class="mdl-navigation__link" href=""><i class="material-icons">insert_chart</i> Analytics</a>
+    </nav>
+</div>
